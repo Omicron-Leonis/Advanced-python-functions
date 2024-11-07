5dd7f154449ffe7c75f177fc309fe080f0e28695
Omicron-Leonis <intisar.mahiyat@gmail.com>
2024-11-07 21:07:54 +0600

    advanced python.cw

---
+++ thats the exit.py
@@ -0,0 +1,14 @@
+# python progress to demonstrate
+# exit
+
+for i in range(10):
+
+    # If the value if i becomes
+    # 5 then the program is forced
+    # to exit
+    if i == 5:
+
+        #prints the exit message
+        print(exit)
+        exit()
+        print(i)
